b8aff18f58b41125182f501651bfe987ee50c84f
Robert Sokolov <sokolov.r123@gmail.com>
2025-08-19 17:26:50 +0200

    Removed redundant asset menu item

--- Editor/Scripts/NetworkInspectorSettingsSO.cs
+++ Editor/Scripts/NetworkInspectorSettingsSO.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace UnityFetch.Editor.Scripts
 {
-    [CreateAssetMenu(fileName = "NetworkInspectorSettingsSO", menuName = "Scriptable Objects/NetworkInspectorSettingsSO")]
     public class NetworkInspectorSettingsSO : ScriptableObject
     {
         public bool clearOnPlay = true;
